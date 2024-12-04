06be8c1b7328dc73733ea9952b59e58b087e516b
guigasprog <guilhermedelgado876@gmail.com>
2024-12-04 14:11:11 -0300

    update: acrescentar interceptors e provide de HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,34 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { TokenInterceptorService } from './shared/interceptors/token/token.interceptor';
+import { ServidorInterceptorService } from './shared/interceptors/server/server.interceptor';
+import { EncryptInterceptor } from './shared/interceptors/encrypt/encrypt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServidorInterceptorService,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: EncryptInterceptor,
+      multi: true,
+    },
+  ],
 };
