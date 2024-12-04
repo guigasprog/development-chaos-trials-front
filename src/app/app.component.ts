514eba5a55d07ebe0687682b7cb624a00f5ca24d
guigasprog <guilhermedelgado876@gmail.com>
2024-12-04 14:11:50 -0300

    update: arrumar de acordo com minha necessidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [AuthService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  constructor(private _router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {
+    if (
+      localStorage.getItem('token') == null &&
+      localStorage.getItem('key') == null
+    ) {
+      this._router.navigate(['login']);
+    } else {
+      this.auth.teste().subscribe((response) => console.log(response));
+    }
+  }
 }
